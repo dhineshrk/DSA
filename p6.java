9f2ce9c56419ac46062b6cc96ff263fd9cd49e39
dhineshrk <dhineshrk77@gmail.com>
2025-03-25 16:29:31 +0530

    Dsa commit

---
+++ p6.java
@@ -0,0 +1,10 @@
+public class p6 {
+    public static void main(String [] args){
+        for(int i = 0; i< 5; i ++){
+            for(int j=5;j>i;j--){
+                System.out.print(5-j+1);
+            }
+            System.out.println();
+        }
+    }
+}
